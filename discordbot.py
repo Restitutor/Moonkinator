cb1b3f4c49f24846943cf747b7a871d2939b043a
Restitutor <nissar@mortemale.org>
2024-05-20 00:53:29 +0000

    Update and rename moonbot.py to discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,12 @@
-#!/home/onfim/no_sync/TTS/venv/bin/python
 #!/usr/bin/env python3
+import asyncio
 from collections import defaultdict
 import discord
 import re
 from wrapper import interact_with_process
 from typing import AsyncGenerator
 import logging
+import shutil
 
 bot = discord.Bot(
     intents=discord.Intents.none()
@@ -14,6 +15,30 @@ bot = discord.Bot(
 )
 
 all_state = defaultdict(str)
+
+CYCY = 1168452148049231934
+EITIFRIE = 804393955533914142
+MOON = 955864224092012644
+RES = 459147358463197185
+ADMINS = {CYCY, EITIFRIE, MOON, RES}
+
+
+async def run_git_pull() -> str:
+    process = await asyncio.create_subprocess_exec(
+        "git",
+        "pull",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd="repository",
+    )
+    shutil.copy("repository/game.py", "game.py")
+
+    stdout, stderr = await process.communicate()
+
+    if stderr:
+        logging.error(stderr)
+
+    return stdout.decode().strip()
 
 
 async def play(state: AsyncGenerator, text: str) -> tuple[str, AsyncGenerator]:
@@ -35,12 +60,20 @@ async def on_message(message):
 
     # Remove non letters
     text = re.sub(r"[^a-z]+", "", message.clean_content.lower())
-    user = message.author.id
 
     if "listakinator" in text:
-        with open('listPlayers.txt') as f:
-            players = f.read().replace('\n', ', ')
+        with open("listPlayers.txt") as f:
+            players = f.read().replace("\n", ", ")
         await message.reply(players)
+        return
+
+    user = message.author.id
+    if "updategame" in text:
+        if user in ADMINS:
+            status = await run_git_pull()
+            await message.reply("Checked for updates.\n" + status)
+        else:
+            await message.reply("You are not an admin.")
         return
 
     if text not in {"yes", "no", "start"}:
@@ -64,4 +97,7 @@ async def on_message(message):
 
 @bot.event
 async def on_ready():
-    print("Started")
+    logger.info("Started")
+
+
+bot.run("TOKEN")
